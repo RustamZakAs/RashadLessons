ecb0678a9675776d7158f2ae12b66b0febe06a22
RustamZakAs <rostik055@gmail.com>
2023-07-13 00:01:18 +0400

    changes

--- Program.cs
+++ Program.cs
@@ -11,40 +11,6 @@ namespace RashadLessons
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!\b ");
-
-            byte _byte = 1;
-            sbyte _sbyte = 127;
-            _sbyte = sbyte.MinValue;
-            _sbyte = sbyte.MinValue;
-
-            //Анологичные переменные с использованием разного написания
-            Int16 int16 = 2;
-            short _int16 = 4;
-
-            UInt16 uint16 = 2;
-            ushort _uint16 = 4;
-
-            Int32 int32 = 4;
-            int _int32 = 8; // _int32 – это переменная, 8 – это литерал
-            int32 = 5 + 2; // 5 + 2 – это выражение, 5 и 2 – это литералы
-            //Литерал — это фиксированное значение, которое записывается непосредственно в исходном коде
-
-            UInt32 uint32 = 4;
-            uint _uint32 = 8;
-
-            Int64 int64 = 8;
-            long _int64 = 16;
-
-            UInt64 uint64 = 8;
-            ulong _uint64 = 16;
-
-            uint _uint = 0U;
-            long _long = 0L;
-            ulong _ulong = 30UL;
-            float _float = 0F;
-            double _double = 0D;
-            decimal _decimal = 0M;
-
             int? dasda; //null
 
             dasda = 0;
@@ -54,13 +20,11 @@ namespace RashadLessons
 
             if (datas.Where(z => z > 3) != null) datas.Where(x => x > 3);
             int? values = datas.Where(x => x > 3)?.FirstOrDefault();
-
-            char _char = ' ';
             //https://metanit.com/sharp/tutorial/2.1.php
 
             //Ссылочные типы данных
-            string _string = String.Empty;
-            String _String = "";
+            string _string = string.Empty;
+            string _String = "";
             _string = "";
             _String = string.Empty;
 
@@ -87,22 +51,83 @@ namespace RashadLessons
             //https://learn.microsoft.com/ru-ru/dotnet/api/system.text.stringbuilder?view=net-7.0
 
             System.GC.Collect(); //Заставляет очистить память //Принудительно начинает сборку мусора
-                                 //https://learn.microsoft.com/ru-ru/dotnet/api/system.gc.collect?view=net-7.0
-                                 //https://learn.microsoft.com/ru-ru/dotnet/standard/garbage-collection/fundamentals
-                                 //https://learn.microsoft.com/ru-ru/dotnet/standard/garbage-collection/fundamentals#:~:text=%D0%94%D0%BB%D1%8F%20%D0%BE%D0%BF%D1%82%D0%B8%D0%BC%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D0%B8%20%D0%BF%D1%80%D0%BE%D0%B8%D0%B7%D0%B2%D0%BE%D0%B4%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D0%B8%20%D1%81%D0%B1%D0%BE%D1%80%D1%89%D0%B8%D0%BA%D0%B0%20%D0%BC%D1%83%D1%81%D0%BE%D1%80%D0%B0,%D0%BD%D0%BE%D0%B2%D1%8B%D0%B5%20%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D1%8B%20%D0%B2%20%D0%BF%D0%BE%D0%BA%D0%BE%D0%BB%D0%B5%D0%BD%D0%B8%D0%B8%200.
-                                 //https://learn.microsoft.com/ru-ru/dotnet/standard/garbage-collection/large-object-heap
 
 
-            //Ключевое слово using имеет два основных применения:
-            //
-            //Директива using создает псевдоним для пространства имен или импортирует типы, определенные в других пространствах имен.
-            //Оператор using определяет область, в конце которой удаляется объект.
+            int peramenntaya = 0;
+            peramenntaya--;
+            Console.WriteLine(peramenntaya);
+            Console.WriteLine(peramenntaya++);
+            Console.WriteLine(peramenntaya--);
+            Console.WriteLine(peramenntaya);
+            peramenntaya = 0;
+            Console.WriteLine(++peramenntaya);
+            Console.WriteLine(--peramenntaya);
 
-            //Область видимости (контекст) переменных и констант
-            //https://metanit.com/sharp/tutorial/2.18.php
+
+            int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+            int asddasdas = intArray.Length / 2;
+            int[] snacala = new int[intArray.Length / 2];
+            for (int i = 0; i < intArray.Length / 2; i++)
+            {
+                snacala[i] = intArray[i];
+                Console.WriteLine($"{snacala[i]}-{intArray[i]}");
+            }
+
+            int[] skonca = new int[intArray.Length / 2];
+            for (int i = intArray.Length - 1, j = 0; i > intArray.Length / 2; i--, j++)
+            {
+                skonca[j] = intArray[i];
+                Console.WriteLine($"{j}-{skonca[j]}/{i}-{intArray[i]}");
+            }
+
+            int[] skonca3 = new int[intArray.Length / 2];
+            for (int i = intArray.Length - 1, j = skonca3.Length - 1; i > intArray.Length / 2; i--, j--)
+            {
+                skonca3[j] = intArray[i];
+                Console.WriteLine($"{j}-{skonca3[j]}/{i}-{intArray[i]}");
+            }
+
+            int[] skonca2 = new int[intArray.Length / 2];
+            for (int i = intArray.Length - 1; i > intArray.Length / 2; i--)
+            {
+                skonca2[i - skonca2.Length - 1] = intArray[i];
+                Console.WriteLine($"{i - skonca2.Length - 1}-{skonca2[i - skonca2.Length - 1]}/{i}-{intArray[i]}");
+            }
+            foreach (var item in skonca2)
+            {
+                Console.Write($"{item},");
+            }
+
+            int a = 2;
+            int b = 2;
+            int c = 2;
+
+            double valuess = 10.0 / 4.0;
+            valuess = valuess - (int)valuess;
+
+            int d = (c--) - (b * a) * (c + 1);
+            Console.WriteLine(d);
+
+            int shotcik = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                int _a = 1;
+                int _b = 1;
+                if ((_a == 1 || _b == 2) && /*and - və*/(_a == 1 || _b == 2)) //(or - və ya)
+                {
+
+                }    
+                    
+                shotcik = shotcik + 2;
+                shotcik += 2;
+                shotcik = ++shotcik;
+                //Operations
+                if (intArray.Length % 50 == 0)
+                {
+                }
+            }
 
 
-            int identicate;  // применение слова identicate в качестве идентификатора
         }
 
         public int one { get; set; }
